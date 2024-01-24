9b1ea710751a248dd7976ac05dec6edc9e8f6603
Chandru <136856772+itischan@users.noreply.github.com>
2024-01-23 23:10:48 -0500

    Add files via upload

---
+++ CrytoTrackerApplication/CrytoTrackerApplication/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CrytoTrackerApplication
+//
+//  Created by ck on 2024-01-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CrytoTrackerApplication/CrytoTrackerApplication/CrytoTrackerApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrytoTrackerApplicationApp.swift
+//  CrytoTrackerApplication
+//
+//  Created by ck on 2024-01-23.
+//
+
+import SwiftUI
+
+@main
+struct CrytoTrackerApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
