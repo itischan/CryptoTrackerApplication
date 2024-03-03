188b67ddb3d582cf914050e8d8f9c52d6e6c0045
itischan <chandrukumaran37@gmail.com>
2024-03-02 23:25:03 -0500

    Added the view for managing portfolio for users #7

---
+++ CrytoTrackerApplication/CrytoTrackerApplication/Core/Home/Views/PortfolioView.swift
@@ -0,0 +1,18 @@
+//
+//  PortfolioView.swift
+//  CrytoTrackerApplication
+//
+//  Created by ck on 2024-03-02.
+//
+
+import SwiftUI
+
+struct PortfolioView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PortfolioView()
+}
---
+++ CrytoTrackerApplication/CrytoTrackerApplication/Services/PortfolioDataService.swift
@@ -0,0 +1,8 @@
+//
+//  PortfolioDataService.swift
+//  CrytoTrackerApplication
+//
+//  Created by ck on 2024-03-02.
+//
+
+import Foundation
