6fbfae4edc1451925828b288539e79a920b58cc5
JudinJoseph <136863555+JudinJoseph@users.noreply.github.com>
2024-04-08 20:56:42 -0400

    Added the view for the launch screen #3

--- CrytoTrackerApplication/CrytoTrackerApplication/CrytoTrackerApplicationApp.swift
+++ CrytoTrackerApplication/CrytoTrackerApplication/CrytoTrackerApplicationApp.swift
@@ -7,16 +7,38 @@
 
 import SwiftUI
 
+
 @main
-struct CrytoTrackerApplicationApp: App {
+struct SwiftfulCryptoApp: App {
+    
     @StateObject private var vm = HomeViewModel()
+    @State private var showLaunchView: Bool = true
+    
+    init() {
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor : UIColor(Color.theme.accent)]
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor : UIColor(Color.theme.accent)]
+        UINavigationBar.appearance().tintColor = UIColor(Color.theme.accent)
+        UITableView.appearance().backgroundColor = UIColor.clear
+    }
+    
     var body: some Scene {
         WindowGroup {
-            NavigationStack{
-                HomeView()
-                    
-            }.environmentObject(vm)
-            
+            ZStack {
+                NavigationView {
+                    HomeView()
+                        .navigationBarHidden(true)
+                }
+                .navigationViewStyle(StackNavigationViewStyle())
+                .environmentObject(vm)
+
+                ZStack {
+                    if showLaunchView {
+                        LaunchView(showLaunchView: $showLaunchView)
+                            .transition(.move(edge: .leading))
+                    }
+                }
+                .zIndex(2.0)
+            }
         }
     }
 }
